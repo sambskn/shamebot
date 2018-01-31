4900d0db988a67be04cd3e4004e82a88a25d313a
sambskn <sambaskin@ou.edu>
2018-01-31 12:33:28 -0600

    adding README, prepping for google sheets integration

---
+++ README.md
@@ -0,0 +1,11 @@
+ShameBot
+========
+Twitter bot that will tweet out names of teams that have dropped out of college ultimate frisbee tourneys with less than 7 days before the event
+
+Built as an exercise in using node.js to tweet data scraped from a public google sheet
+
+I host this on heroku, and throw all those important authorization tokens and whatnot in the ENV, y'know, for security and stuff
+
+used these tutorials as starting points if you want to make your own
+- [Google's Node.js Quickstart Guide for their Google Sheets API](https://developers.google.com/sheets/api/quickstart/nodejs)
+- [BotWiki's Glitch Guide to Creating a Twitter Bot](https://glitch.com/edit/#!/twitterbot?path=README.md:1:0)
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var path = require('path'),
     express = require('express'),
     app = express(),   
     Twit = require('twit'),
+    fs = require('fs'),
+    readline = require('readline'),
+    google = require('googleapis'),
+    googleAuth = require('google-auth-library'),
     config = {
     /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */      
       twitter: {
@@ -14,12 +18,15 @@ var path = require('path'),
     },
     T = new Twit(config.twitter);
 
+    //need to implement google sheets stuff here
+
+
+
 app.use(express.static('public'));
 
 /* You can use uptimerobot.com or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
 
 app.all("/" + process.env.BOT_ENDPOINT, function (request, response) {
-/* The example below tweets out "Hello world!". */
   var resp = response;
   var tweet  = "";
   var date =  new Date().getTime();
